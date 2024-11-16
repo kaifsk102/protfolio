52bfc68741a1a2f03b731cba9c45d3fd93d81e3d
MD KAIF SK <93993949+kaifsk102@users.noreply.github.com>
2024-11-16 09:22:15 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,16 @@
 module.exports = {
     content: ["./{html,js}"],
     theme: {
+        screens: {
+      'sm': '400px',
+      // => @media (min-width: 576px) { ... }
+
+      'md': '800px',
+      // => @media (min-width: 960px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1440px) { ... }
+    },
       extend: {},
     },
     plugins: [],
